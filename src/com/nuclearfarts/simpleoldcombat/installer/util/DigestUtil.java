2adaa1494ba1e71b9d1dc3635d0effe2ac20f679
Nuclearfarts <Nuclearfarts@users.noreply.github.com>
2019-05-14 18:28:33 -0400

    compatibility with newer JREs
    So Oracle just removed the only JRE way to turn a byte array into a hex string and didn't provide an alternative.
    
    thanks oracle.

--- src/com/nuclearfarts/simpleoldcombat/installer/util/DigestUtil.java
+++ src/com/nuclearfarts/simpleoldcombat/installer/util/DigestUtil.java
@@ -7,26 +7,43 @@ import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
-
 public class DigestUtil {
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
 	public static String sha1(File file) throws IOException {
 		MessageDigest hasher = null;
 		try {
 			hasher = MessageDigest.getInstance("SHA-1");
-		} catch (NoSuchAlgorithmException e) {e.printStackTrace();}
-	    try(InputStream fis = new FileInputStream(file)) {
-	    	int n = 0;
-	    	byte[] buffer = new byte[8192];
-	    	while (n != -1) {
-	        	n = fis.read(buffer);
-	        	if (n > 0) {
-	        		hasher.update(buffer, 0, n);
-	        	}
-	    	}
-	    }
-	    return new HexBinaryAdapter().marshal(hasher.digest());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		try (InputStream fis = new FileInputStream(file)) {
+			int n = 0;
+			byte[] buffer = new byte[8192];
+			while (n != -1) {
+				n = fis.read(buffer);
+				if (n > 0) {
+					hasher.update(buffer, 0, n);
+				}
+			}
+		}
+		return bytesToHex(hasher.digest());
 	}
 
+	/**
+	 * Thanks for removing DataTypeConverter from the standard JDK with NO
+	 * ALTERNATIVE BUT TO WRITE YOUR OWN, Oracle. People TOTALLY wanted to lose a
+	 * basic feature like converting a byte array to a hex string.
+	 * 
+	 * And they wonder why everyone hates them.
+	 */
+	public static String bytesToHex(byte[] bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
+	}
 }
